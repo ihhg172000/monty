2e83f7e7f4226674a8ddafce5643a75b812684a1
TanjereeN98 <ahmadcyc35@gmail.com>
2023-06-22 05:38:56 +0300

    Adding pchar Opcode

--- pchar.c
+++ pchar.c
@@ -9,12 +9,10 @@
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	stack_t *top = *stack;
-
-	if (!top)
+	if (!*stack)
 		exit_with_ferror("L%u: can't pchar, stack empty\n", line_number);
-	if (top->n >= 0 && top->n <= 127)
-		printf("%c\n", top->n);
+	if ((*stack)->n >= 0 && (*stack)->n <= 127)
+		printf("%c\n", (*stack)->n);
 	else
 		exit_with_ferror("L%u: can't pchar, value out of range\n", line_number);
 }
