71c4c850df8631f76cf95237ec2d5d92922f144e
TanjereeN98 <ahmadcyc35@gmail.com>
2023-06-22 05:28:53 +0300

    Adding pchar Opcode

--- monty.h
+++ monty.h
@@ -45,6 +45,8 @@ void (*find_instruction())(stack_t **stack, unsigned int line_number);
 
 void interpret(void);
 
+void pchar(stack_t **stack, unsigned int line_number);
+
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
